ef9d0c4ca7c137dfb2bd99ff8fb31b37f0a91998
Yunis Huseynzade <yunisdev.04@gmail.com>
2021-04-24 16:52:12 +0400

    v1.1.5

--- snakenv/__main__.py
+++ snakenv/__main__.py
@@ -5,23 +5,26 @@ from dotenv import dotenv_values
 
 
 def add_to_global(key, value):
-    from sys import platform
-    if platform in ["linux", "linux2", "darwin"]:
-        os.system(f'export {key}="{value}"')
-    elif platform == "win32":
-        os.system(f'set {key}="{value}"')
+    os.environ[key] = value
+    # from sys import platform
+    # if platform in ["linux", "linux2", "darwin"]:
+    #     os.system(f'export {key}="{value}"')
+    # elif platform == "win32":
+    #     os.system(f'set {key}="{value}"')
 
 
 @click.command()
 @click.argument("env_name", default="")
 @click.option('--not-create', is_flag=True)
-def main(env_name, not_create):
+@click.option('--command','-c')
+def main(env_name, not_create,command):
     env_file = env_name+(".env" if not ".env" in env_name else "")
     if isfile(env_file):
         env_data = dotenv_values(env_file)
         for key, value in env_data.items():
             add_to_global(key, value)
             print(f"[snakenv] {key} -> {value}")
+        os.system(command)
     else:
         if not not_create:
             with open(env_file, "w") as f:
